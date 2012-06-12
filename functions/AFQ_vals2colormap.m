e5f78bddcf18c7a27dd300156136412b2f94ba74
jyeatman <jyeatman@stanford.edu>
2012-06-12 16:45:59 -0700

    Now AFQ_vals2colormap handles nans

--- functions/AFQ_vals2colormap.m
+++ functions/AFQ_vals2colormap.m
@@ -33,6 +33,8 @@ cmap = eval([colormap '(256)']);
 vals(vals < crange(1)) = crange(1);
 vals(vals > crange(2)) = crange(2);
 valsN = round(((vals - crange(1)) ./ diff(crange)) .* 255)+1;
+% Convert any nans to ones
+valsN(isnan(valsN)) = 1;
 % Convert the normalized values to the RGB values of the colormap
 rgb = cmap(valsN, :);
 
