61c74a8793ec110a5f25fbda6f350184b94bcaf7
jyeatman <jyeatman@stanford.edu>
2012-09-05 12:51:16 -0700

    Added comments

--- functions/AFQ_TractProfileAlign.m
+++ functions/AFQ_TractProfileAlign.m
@@ -3,18 +3,33 @@ function [params,mapv1,mapv1b] = AFQ_TractProfileAlign(v1,v2,numanchor)
 %
 % function [params,mapv1,mapv1b] = AFQ_TractProfileAlign(v1,v2,numanchor)
 %
-% <v1>,<v2> are vectors of the same length
-% <numanchor> is the number of anchor points to use.
+% This function will coregister the Tract Profile in vector 1 (v1) to the
+% Tract Profile in vector 2 (v2).  This is done by using searching for an
+% anchor point on v1 and a mapping of this anchor point to an equivalent
+% point on v2. v1 is then interpolated with respect to this anchor point so
+% that the two vectors match.  The search may have problems with local
+% minimums when the number of anchor points is > 1.
 %
+% Inputs:
 %
-% example:
-% v1 = evalgaussian1d([20 5 1 0],1:100);
-% v2 = evalgaussian1d([40 20 3 0],1:100);
-% [params,mapv1,mapv1b] = AFQ_TractProfileAlign(v1,v2,2);
-% figure; hold on;
-% plot(v1,'r-');
-% plot(v2,'g-');
-% plot(mapv1,'b-');
+% v1        - An input vector to coregister
+% v2        - Target vector. v1 will be coregisterd to v2. 
+% numanchor - The number of anchor points on v1 to map to v2. 
+%
+% Outputs:
+%
+% params    - Parameters of the transform.  These can be plugged into
+%             AFQ_TractProfileInterp to resample v1.
+% mapv1     - v1 coregistered to v2 just based on stretching the vector
+%             along the x axis
+% mapv1b    - v1 coregistered to v2 based on stretching it scaling it and
+%             offsetting it
+%
+% Example:
+%
+% v1 = evalgaussian1d([20 5 1 0],1:100); v2 = evalgaussian1d([40
+% 20 3 0],1:100); [params,mapv1,mapv1b] = AFQ_TractProfileAlign(v1,v2,2);
+% figure; hold on; plot(v1,'r-'); plot(v2,'g-'); plot(mapv1,'b-');
 % plot(mapv1b,'c-');
 
 % define options for the optimization
@@ -30,14 +45,12 @@ h = pinv([v1' ones(length(v1),1)])*v2';  % first parameter is scale, second is o
 % construct final seed
 params0 = [h(:)' params0(:)'];
 
-% define bounds
-% paramslb = zeros(1,numanchor);
-% paramsub = ones(1,numanchor);
+% define bounds paramslb = zeros(1,numanchor); paramsub =
+% ones(1,numanchor);
 paramslb = [-Inf -Inf zeros(1,2*numanchor)];
 paramsub = [Inf Inf ones(1,2*numanchor)];
 
-% Fit parameters
-% OLD: sqrt(1-nanreplace(corr(mapping(a,v1)',v2'),-1))
+% Fit parameters OLD: sqrt(1-nanreplace(corr(mapping(a,v1)',v2'),-1))
 [params,d,d,exitflag,output] = ...
     lsqnonlin(@(params) v2 - (params(1)*AFQ_TractProfileInterp(params(3:end),v1) + params(2)) , ...
     params0,paramslb,paramsub,options);
