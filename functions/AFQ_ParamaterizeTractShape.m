572cc1451dc0256dac0c2f7ca991e420d7f1e65f
jyeatman <jyeatman@stanford.edu>
2012-09-13 18:44:50 -0700

    Added a function to paramaterize curvature and torsion along the tract profile

---
+++ functions/AFQ_ParamaterizeTractShape.m
@@ -0,0 +1,98 @@
+function [curvWA torsWA TractProfile] = AFQ_ParamaterizeTractShape(fg, TractProfile)
+% Paramaterize curvature and torsion along a tract profile
+%
+% [curvWA torsWA TractProfile] = AFQ_ParamaterizeTractShape(fg, TractProfile)
+%
+% Inputs:
+% fg           - Fiber Group. This can be a structure containing multiple
+%                fiber groups where each fiber group is an entry in the
+%                structure
+% TractProfile - Tract Profile for the fiber group. If one is not entered
+%                then it will be calculated
+%
+% Outputs:
+% curvWA       - Curvature of the tract core calculated by taking a
+%                weighted average of curvature on each fiber.
+% torsWA       - Torsion of the tract core calculated by taking a weighted
+%                average of torsion on each fiber.
+% TractProfile - Tract Profile containing these measurements
+%
+% Written by Jason D. Yeatman September 2012
+
+%% Argument checking
+if ~exist('fg','var') || isempty(fg)
+    error('Please provide a fiber group')
+elseif isfield(fg,'subgroup') && isfield(fg,'subgroupNames') && length(fg.subgroupNames) > 1
+    fg = fg2Array(fg);
+end
+
+if ~exist('TractProfile','var') || isempty(TractProfile)
+    % Build a tract profile of the fiber group if one was not passed in
+    numNodes = 100;
+    % If there are multiple fiber groups stored within fg then loop over them
+    for ii = 1:length(fg)
+        [SuperFiber, fg(ii)]= dtiComputeSuperFiberRepresentation(fg(ii), [], numNodes);
+        TractProfile(ii) = AFQ_CreateTractProfile('name',fg.name,'superfiber',SuperFiber);
+    end
+elseif length(TractProfile) == length(fg)
+    % Number of nodes in the tract profile
+    numNodes = length(TractProfile(ii).coords.acpc);
+    for ii = 1:length(fg)
+        % Resample and reorient fibers so that each fiber starts and ends in the
+        % same place
+        fg(ii) = dtiReorientFibers(fg, numNodes);
+    end
+elseif length(TractProfile) ~= length(fg)
+    error('TractProfile and fg must have the same number of fiber groups')
+end
+
+%% Loop over the fiber groups in fg and calculate torsion and curvature
+curvWA = zeros(numNodes,length(fg));
+torsWA = zeros(numNodes,length(fg));
+for jj = 1:length(fg)
+    % Initialize variables
+    numfibers = length(fg(jj).fibers);
+    curv = zeros(numNodes,numfibers);
+    tors = zeros(numNodes,numfibers);
+    weights = zeros(numNodes,numfibers);
+    
+    % Calculate curvature and torsion for each node on each fiber
+    for ii = 1:numfibers
+        [~,~,~,curv(:,ii),tors(:,ii)] = frenet2(fg(jj).fibers{ii}(1,:)',fg(jj).fibers{ii}(2,:)',fg(jj).fibers{ii}(3,:)');
+    end
+    
+    % Each fiber is represented by numberOfNodes, so can easily loop over 1st
+    % node, 2nd, etc...
+    fc = horzcat(fg(jj).fibers{:})';
+    
+    % Calculate the weights to be applied to each fibers curvature estimates.
+    % These weights are based on the fiber's mahalanobis distance from the
+    % fiber core. They will be used to weight that fibers contribution to the
+    % core measurement
+    for node=1:numNodes
+        % Compute gaussian weights y = mvnpdf(X,mu,SIGMA);
+        % Returns the density of the multivariate normal distribution with zero
+        % mean and identity covariance matrix, evaluated at each row of X.
+        X=fc((1:numNodes:numfibers*numNodes)+(node-1), :);
+        sigma = [SuperFiber.fibervarcovs{1}(1:3, node)'; ...
+            0 SuperFiber.fibervarcovs{1}(4:5, node)';...
+            0 0 SuperFiber.fibervarcovs{1}(6, node)'];
+        sigma = sigma + sigma' - diag(diag(sigma));
+        mu    = SuperFiber.fibers{1}(:, node)';
+        % Weights for the given node.
+        weights(node, :) = mvnpdf(X,mu,sigma)';
+    end
+    
+    % The weights for each node should add up to one across fibers.
+    weightsNormalized = weights./(repmat(sum(weights, 2), [1 numfibers]));
+    
+    % Weight each curvature and torsion calculation by its distance from the
+    % core
+    curvWA(:,jj) = sum(curv.*weightsNormalized,2);
+    torsWA(:,jj) = sum(tors.*weightsNormalized,2);
+    
+    % Assign values to the tract profile structure
+    TractProfile(jj).fibercurvature = curvWA(:,jj);
+    TractProfile(jj).fibertorsion   = torsWA(:,jj);
+    
+end
