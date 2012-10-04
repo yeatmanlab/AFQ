696c2d8a0b0ddb6656914415240df3cfcbe6e384
jyeatman <jyeatman@stanford.edu>
2012-10-04 12:10:45 -0700

    updated tutorial

--- AFQ_example_GroupComparison.m
+++ AFQ_example_GroupComparison.m
@@ -8,11 +8,13 @@ sub_dirs = {[AFQdata '/patient_01/dti30'], [AFQdata '/patient_02/dti30']...
     [AFQdata '/control_02/dti30'], [AFQdata '/control_03/dti30']};
 % Create a vector of 0s and 1s defining who is a patient and a control
 sub_group = [1, 1, 1, 0, 0, 0];
-% Run AFQ in test mode to save time. No inputs are needed to run AFQ
-% with the default settings. AFQ_Create builds the afq structure. This
-% will also be done automatically by AFQ_run if the user does not wish
-% to modify any parameters
-afq = AFQ_Create('run_mode','test', 'sub_dirs', sub_dirs, 'sub_group', sub_group);
+% Run AFQ in test mode to save time and do not show figures. No inputs are
+% needed to run AFQ with the default settings. AFQ_Create builds the afq
+% structure. This will also be done automatically by AFQ_run if the user
+% does not wish to modify any parameters
+afq = AFQ_Create('run_mode','test', 'sub_dirs', sub_dirs, 'sub_group',...
+    sub_group, 'showfigs',true);
+% Run AFQ to generate the fiber tracts
 [afq patient_data control_data norms abn abnTracts] = AFQ_run(sub_dirs, sub_group, afq);
 
 %% Run a t-test to compare FA along each fiber tract for patients vs. controls
@@ -34,13 +36,27 @@ dt = dtiLoadDt6(fullfile(sub_dirs{3},'dt6.mat'));
 numNodes = 100;
 [fa md rd ad cl TractProfile] = AFQ_ComputeTractProperties(fg,dt,numNodes);
 % Add the pvalues and T statistics from the group comparison to the tract
-% profile
+% profile. This same code could be used to add correlation coeficients or
+% other statistics
 for jj = 1:20
     TractProfile(jj) = AFQ_TractProfileSet(TractProfile(jj),'vals','pval',p(jj,:));
     TractProfile(jj) = AFQ_TractProfileSet(TractProfile(jj),'vals','Tstat',Tstats(jj).tstat);
 end
 
-%% Render the tract profiles of T statistics
+%% Render the tract profiles of p values
 
+% The hot colormap is a good one because it will allow us to make regions
+% of the profile where p>0.05 black.
+cmap = 'hot';
+% Set the color range to black out non significant regions of the tract. We
+% will render the T-stat profile. T values below 1.96 correspond to p>0.05.
+crange = [1.96 3];
+% Set the number of fibers to render. More fibers takes longer
+numfibers = 100;
 % Render the left corticospinal tract with a Tract Profile of T statistics 
-AFQ_RenderFibers(fg(3),'tractprofile',TractProfile(3),'val','pval','numfibers',100)
+AFQ_RenderFibers(fg(3),'tractprofile',TractProfile(3),'val','Tstat',...
+    'numfibers',numfibers,'cmap',cmap,'crange',crange);
+% Add the defining ROIs to the rendering
+[roi1 roi2] = AFQ_LoadROIs(3,sub_dirs{3});
+AFQ_RenderRoi(roi1);
+AFQ_RenderRoi(roi2);
