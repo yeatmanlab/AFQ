5ecd856b0b861949089d6816b94a02d4fa6be985
jyeatman <jyeatman@stanford.edu>
2012-11-17 11:16:09 -0800

    new tutorials

---
+++ tutorials/tutorial2_DataPlotsStatsExport.m
@@ -0,0 +1,9 @@
+%% Fiber tract heatmap plot
+
+%% Plot individual subjectsagains control group norms
+
+%% Plot a group comparison
+
+%% Compute multiple comparison correction
+
+%% Export data as an excel spreadsheet
