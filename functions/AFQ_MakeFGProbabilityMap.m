39d3a79582fc63fc08f9f5c0ae3a643c7219adc0
jyeatman <jyeatman@stanford.edu>
2012-11-18 14:23:38 -0800

    started building a function to make fiber group probability maps

---
+++ functions/AFQ_MakeFGProbabilityMap.m
@@ -0,0 +1,9 @@
+function im = AFQ_MakeFGProbabilityMap(dtPaths,fgPaths, writeImage)
+% THIS FUNCTION IS STILL BEING DEVELOPED
+% Make a fiber tract probability map (nifti image) from fiber group
+% segmentations in a sample of subjects
+%
+% im = AFQ_MakeFGProbabilityMap(dtPaths,fgPaths, [writeImage = true])
+%
+%
+% Copyright Jason D. Yeatman, November 2012
