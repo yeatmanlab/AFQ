ebda8f27f122ba615ab1c4dc27669f435a04bae2
jyeatman <jyeatman@stanford.edu>
2012-11-18 14:24:03 -0800

    started building a function to segment the callosum

---
+++ functions/AFQ_SegmentCallosum.m
@@ -0,0 +1,9 @@
+function fg_classified = AFQ_SegmentCallosum(dt, wholeBrain, varargin)
+% THIS FUNCTION IS STILL BEING DEVELOPED
+% Segment the callosal fibers into 7 segments
+%
+% fg_classified = AFQ_SegmentCallosum(dt, wholeBrainFG, varargin)
+%
+%
+%
+% Copyright Jason D. Yeatman November 2012
