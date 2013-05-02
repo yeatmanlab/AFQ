456fe5a576cb55d4b8fa1668c1a36cf05603a098
jyeatman <jyeatman@stanford.edu>
2013-05-01 17:23:27 -0700

    finished writing this function

--- 3Dvis/AFQ_MakeFiberGroupMontage.m
+++ 3Dvis/AFQ_MakeFiberGroupMontage.m
@@ -1,5 +1,5 @@
-function AFQ_MakeFiberGroupMontage(afq, fgNames, fgColors, subNums, plotDims)
-
+function AFQ_MakeFiberGroupMontage(afq, fgNames, fgColors, subNums, plotDims, numfibers)
+% Make a montage of fiber group renderings across subjects
 
 %% Argument checking
 % By default render for all subjects
@@ -13,6 +13,10 @@ end
 % By default render fibers in a 3x3 subplot window
 if ~exist('plotDims','var') || isempty(plotDims)
     plotDims = [3 3];
+end
+% By default render 200 fibers
+if ~exist('numfibers','var') || isempty(numfibers)
+    numfibers = 200;
 end
 
 % Calculate the number of plots in a window
@@ -40,7 +44,7 @@ for ii = subNums
     % Load fiber groups
     for jj = 1:length(fgNames)
         % Load fibers
-        fg = dtiLoadFiberGroup(AFQ_get(afq,[prefix(fgNames{jj}) 'path'],ii));
+        fg = AFQ_get(afq,[prefix(fgNames{jj}) 'fg'],ii);
         % Designate the color of these fibers
         colors = vertcat(colors,repmat(fgColors(jj,:),length(fg.fibers),1));
         % Concatenate fibers
@@ -49,6 +53,6 @@ for ii = subNums
     % Put concatenated fibers into a fiber group
     fg = dtiNewFiberGroup('all',[],[],[],fgCombined);
     % Render the fibers
-    AFQ_RenderFibers(fg, 'color', colors, 'numfibers',300, 'subplot',[3 3 c]);
-
+    AFQ_RenderFibers(fg, 'color', colors, 'numfibers',numfibers,'tubes',0,'subplot',[plotDims(1) plotDims(2) c]);
+    title(sprintf('Subject #%d',ii));
 end
