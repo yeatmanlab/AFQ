74032f58efb5fef059906edf9247634975d60ab8
jyeatman <jyeatman@stanford.edu>
2013-07-10 16:01:51 -0700

    updated vector plots with error bars

--- utilities/errorbarvectorplot.m
+++ utilities/errorbarvectorplot.m
@@ -9,7 +9,7 @@ g = g(g~=0);
 if notDefined('colors')
     colors = hsv(length(g)).*.8;
 end
-
+x0 = 1:size(data,2);
 for ii = 1:length(g)
     % Pull out this groups data
     vals = data(group==g(ii),:);
@@ -22,8 +22,11 @@ for ii = 1:length(g)
     % standard error of the mean at each node
     se = sd./sqrt(n);
     % plot the mean
-    h(1,ii) = plot(m,'-','Color',colors(ii,:),'linewidth',3);
+    h(1,ii) = plot(m,'-','Color',colors(ii,:),'linewidth',1);
     % plot the confidence interval
-    h(3,ii) = plot(m+se,'--','Color',colors(ii,:));
-    h(2,ii) = plot(m-se,'--','Color',colors(ii,:));
+    
+    fill([x0 fliplr(x0)],[m+se fliplr(m-se)],colors(ii,:),'facealpha',.5,'edgealpha',0)
+    
+    %     h(3,ii) = plot(m+se,'--','Color',colors(ii,:));
+    %     h(2,ii) = plot(m-se,'--','Color',colors(ii,:));
 end
