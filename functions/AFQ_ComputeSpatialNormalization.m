d0ee8e633ee2eabfaf5272164ff10364b34376e2
jyeatman <jyeatman@stanford.edu>
2013-07-11 09:40:36 -0700

    added new functions and templates

---
+++ functions/AFQ_ComputeSpatialNormalization.m
@@ -0,0 +1,18 @@
+function AFQ_ComputeSpatialNormalization(afq)
+% Compute spatial normalization to MNI template for each subject
+
+
+if AFQ_get(afq, 'use ANTS')
+    fprintf('\n Computing spatial normalization with ANTS\n')
+    sub_dirs = AFQ_get(afq,'sub_dirs')
+    for ii = 1:length(sub_dirs)
+        b0File = fullfile(sub_dirs{ii},'bin','b0.nii.gz');
+        tdir = fullfile(AFQ_directories,'templates','mni_icbm152_nlin_asym_09a_nifti');
+        template = fullfile(tdir,'mni_icbm152_t2_tal_nlin_asym_09a.nii');
+        ANTS_normalize(b0File,template);
+        warp = fullfile(sub_dirs{ii},'bin','b0Warp.nii.gz');
+        invwarp = fullfile(sub_dirs{ii},'bin','b0InverseWarp.nii.gz');
+        afq = AFQ_set(afq,'ants warp', warp, ii);
+        afq = AFQ_set(afq,'ants inverse warp', invwarp, ii);
+    end
+end
