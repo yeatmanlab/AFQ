357f0564fb6a8c63cee92d26d0f97745b7fed1f2
jyeatman <jyeatman@stanford.edu>
2013-10-02 15:17:29 -0700

    meshCut finally works properly

--- 3Dvis/AFQ_meshCut.m
+++ 3Dvis/AFQ_meshCut.m
@@ -10,14 +10,26 @@ cp = diff(plane);
 
 % X clip
 if cp(1) >0
+    % These are the vertices to remove. All the ones that are greater than
+    % the first plane but less than the second plane
     vInds = msh.tr.vertices(:,1) > plane(1,1) & msh.tr.vertices(:,1) < plane(2,1);
     % Remove vertices and corresponding colors within the clip range
     msh.tr.vertices = msh.tr.vertices(~vInds,:);
     msh.tr.FaceVertexCData = msh.tr.FaceVertexCData(~vInds,:);
-    % Find face corresponding to these vertices
+    % Find faces that include a removed vertex
+    fInds = [];
     fInds(:,1) = ismember(msh.tr.faces(:,1),find(vInds));
     fInds(:,2) = ismember(msh.tr.faces(:,2),find(vInds));
     fInds(:,3) = ismember(msh.tr.faces(:,3),find(vInds));
+    % Turn into a logical vector
+    Fkeep = sum(fInds,2) == 0;
     % Remove any face that goes to a removed vertex
-    msh.tr.faces = msh.tr.faces(sum(fInds,2)==0);
+    msh.tr.faces = msh.tr.faces(Fkeep,:);
+    % Now this part is a bit tricky. We need to recompute which rows of
+    % vertices each face corresponds to since we have removed a bunch of
+    % rows
+    offset = cumsum(vInds);
+    for ii = 1:numel(msh.tr.faces)
+        msh.tr.faces(ii) = msh.tr.faces(ii) - offset(msh.tr.faces(ii));
+    end
 end
