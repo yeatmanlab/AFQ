d2a73f9418868a8e08336c91fe4d9fdcfefc7370
jyeatman <jyeatman@stanford.edu>
2013-11-29 12:06:38 -0800

    updated plotpatientmeans to include all types of output figures

--- functions/AFQ_PlotPatientMeans.m
+++ functions/AFQ_PlotPatientMeans.m
@@ -1,4 +1,4 @@
-function AFQ_PlotPatientMeans(afq_patient,afq_controls,valname,nodes,varargin)
+function AFQ_PlotPatientMeans(afq_patient,afq_controls,valname,nodes,outdir,varargin)
 % Plot patient data against controls
 %
 %
@@ -17,6 +17,14 @@ if notDefined('nodes')
     nodes = 20:80;
 end
 
+% Define output directory
+if notDefined('outdir')
+    outdir = pwd;
+end
+
+% Define whether plots should be in one window or separate
+subplots = 0;
+
 % Open a new figure window for each plot
 f1 = figure; hold('on');
 f2 = figure; hold('on');
@@ -29,6 +37,17 @@ end
 % Get number of fiber groups and their names
 nfg = AFQ_get(afq_patient,'nfg');
 fgNames = AFQ_get(afq_patient,'fgnames');
+
+% Load the fiber group for the patient
+fg_p = AFQ_get(afq_patient,'clean fg',1);
+
+% Set the views for each fiber group
+fgviews = {'leftsag', 'rightsag', 'leftsag', 'rightsag', ...
+    'leftsag', 'rightsag', 'leftsag', 'rightsage', 'axial', 'axial',...
+    'leftsag', 'rightsag', 'leftsag', 'rightsag',  'leftsag', 'rightsag'...
+    'leftsag', 'rightsag', 'leftsag', 'rightsag'};
+% Set the colormap and color range for the renderings
+cmap = 'jet'; crange = [-4 4];
 % Loop over each fiber group
 for ii = 1:nfg
     % Get the values for the patient and compute the mean
@@ -36,7 +55,7 @@ for ii = 1:nfg
     % Remove nodes that are not going to be analyzed
     vals_p = vals_p(nodes);
     vals_pm = nanmean(vals_p);
-
+    
     % Get the value for each control and compute the mean
     vals_c = AFQ_get(afq_controls,fgNames{ii},valname);
     vals_c = vals_c(:,nodes);
@@ -48,6 +67,10 @@ for ii = 1:nfg
         mdata = AFQ_get(afq_controls,'metadata',varargin{kk});
         mrange = varargin{kk+1};
         use(:,(kk+1)./2) = mdata >= mrange(1) & mdata <= mrange(2);
+    end
+    % If no metadata was defined use all the controls
+    if notDefined('use')
+        use = ones(length(afq_controls.sub_dirs),1);
     end
     use = all(use,2);
     % Remove values for unwanted subjects
@@ -69,11 +92,18 @@ for ii = 1:nfg
     %fill(x,y3, [.8 .8 .8],'edgecolor',[0 0 0]);
     fill(x,y2, [.6 .6 .6],'edgecolor',[0 0 0]);
     fill(x,y1,[.4 .4 .4] ,'edgecolor',[0 0 0]);
+    % Define the color of the point for the fiber group based on its zscore
+    tractcol = vals2colormap((vals_pm - m)./sd,cmap,crange);
     % Plot patient
-    plot(ii, vals_pm,'ko', 'markerfacecolor',[1 0 0]);
+    plot(ii, vals_pm,'ko', 'markerfacecolor',tractcol);
     
-    % And plot tract profiles as well
-    figure(f2); subplot(10,2,ii); hold('on');
+    %% Plot tract profile
+    if subplots == 1
+        % Tract profiles can either be separate or within the same figure
+        figure(f2); subplot(10,2,ii); hold('on');
+    else
+        figure; hold('on');
+    end
     y1 = [m_tp-sd_tp fliplr(m_tp+sd_tp)];
     y2 = [m_tp-2*sd_tp fliplr(m_tp+2*sd_tp)];
     y3 = [m_tp-3*sd_tp fliplr(m_tp+3*sd_tp)];
@@ -84,10 +114,50 @@ for ii = 1:nfg
     plot(nodes,m_tp,'-k','linewidth',3);
     % Plot patient
     plot(nodes, vals_p, '-r', 'linewidth',3);
-    title([fgNames{ii} ' - ' valtitle]);axis tight
+    title([fgNames{ii} ' - ' valtitle]);
+    ylabel(valtitle); xlabel('Location');
+    
+    % Set the figures size etc
+    set(gcf,'units','inches','position',[1 1 8 5],'paperpositionmode','auto')
+    % Save an image
+    print(gcf,'-djpeg', '-r150', sprintf('tp_%d',ii));
+        
+    %% Render the tract profile
+    
+    % Check if the fiber group is empty
+    if ~isempty(fg_p(ii).fibers)
+        % Get the tract profile
+        tp = afq_patient.TractProfiles(ii);
+        % zscore the values along the tract profile length
+        vals_z = (vals_p - m_tp)./sd_tp;
+        % Add the zscores to the tract profile structure
+        tp = AFQ_TractProfileSet(tp,'vals','zscore',vals_z);
+        
+        % Remove the unwanted nodes from the tract profile before rendering
+        tp.coords.acpc = tp.coords.acpc(:,nodes);
+        
+        % Render the tract profile
+        AFQ_RenderFibers(fg_p(ii),'tractprofile', tp,...
+            'val', 'zscore', 'numfibers', 200, 'cmap', cmap, ...
+            'crange', crange, 'camera', fgviews{ii}, 'radius', [.5 5]); 
+        axis('off');
+        
+        % Set the figures size etc
+        set(gcf,'units','inches','position',[1 1 8 5],'paperpositionmode','auto')
+        % Save an image
+        print(gcf,'-djpeg', '-r150', sprintf('fg_%d',ii));
+        % Close the figure
+        close(gcf);
+    end
+    
 end
 
+% Save out the figure of the tract means
 figure(f1)
-set(gca,'xtick',1:nfg,'xticklabel',fgNames,'xlim',[0 nfg+1])
-rotateXLabels(gca,45)
-ylabel(valtitle)
+set(gca,'xtick',1:nfg,'xticklabel',fgNames,'xlim',[0 nfg+1]);
+rotateXLabels(gca,45);
+ylabel(valtitle);
+% Set the figures size etc
+set(f1,'units','inches','position',[1 1 15 5],'paperpositionmode','auto')
+% Save an image
+print(f1,'-djpeg', '-r150', sprintf('means'));
