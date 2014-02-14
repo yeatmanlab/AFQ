93cc39af9b59d71ed3e5c904da71f0c82e2cf392
Michael Perry <lmperry@stanford.edu>
2014-02-14 13:55:14 -0800

    ENH: Now returns the exit code and the files that will be used for processing. Also now does not exit by default

--- nims/nims_checkData.m
+++ nims/nims_checkData.m
@@ -1,14 +1,15 @@
-function nims_checkData(path)
+function [thecode, files] = nims_checkData(path,doexit)
 % 
 %  status = nims_checkData(path)
 % 
-% Given a path, returns a status where 1 indicates that we have all the
-% files needed to process a particular data set and 0 means that we don't.
-% Perhaps 2 will mean that we have only part of the files needed.
+% Given a path, returns a status where 111 indicates that we have all the
+% files needed to process a particular data set and 000 means we don't.
+% 2222 will mean that we have only part of the files needed.
 % 
 % 
 % 
-% (C) Stanford University, Vista Lab [2014]
+% 
+% (C) Stanford University, Vista Lab [2014] - LMPERRY
 % 
 
 
@@ -18,14 +19,13 @@ if notDefined('path') || ~exist(path,'dir')
     if path == 0; DWI = 0; mrQ = 0; clear path; return; end
 end
 
+if notDefined('exit')
+    doexit = 0;
+end
 
 
 %% Get a list all the relevant DWI files
 dw = getDwiFilesStruct(path);
-
-% Create the initial structure
-mrQ.name = tempname;
-mrQ.RawDir = path;
 
 % Check for the existence of dwData
 if isempty(dw)
@@ -44,6 +44,10 @@ end
 %% Given mrQ.RawDir, the following function will return all the necessarry
 % paths to required nifti files in the mrQ.inputData_spgr and
 % mrQ.inputData_seir structures (maybe this should be part of mrQ create).
+% Create the initial structure
+
+mrQ.name = tempname;
+mrQ.RawDir = path;
 mrq = mrQ_initInputData(mrQ);
 
 
@@ -69,6 +73,14 @@ else
 end
 
 
+%% Return the files
+
+files = {};
+files.dw = dw;
+files.spgr = mrq.inputdata_spgr.name;
+files.seir = mrq.inputdata_seir.name;
+
+
 %% Set the exit code and exit
 
 decision = mrQ+DWI;
@@ -82,8 +94,14 @@ switch decision
         thecode = 000; %  0 = no go
 end
 
-fprintf('Exiting - status = %d\n',thecode);
-% exit(thecode);
+% If the user wanted to exit, then go for it - close matlab and return the
+% exit code to the terminal. By default we don't do this.
+if doexit == 1
+    fprintf('Exiting - status = %d\n',thecode);
+    exit(thecode);
+end
+
+
 
 return
 
