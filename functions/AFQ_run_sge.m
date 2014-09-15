21d01e237858cff0e12f218df645fcd4b3608a86
jyeatman <jyeatman@stanford.edu>
2014-09-15 14:55:32 -0700

    added support for matlab parallel toolbox

--- functions/AFQ_run_sge.m
+++ functions/AFQ_run_sge.m
@@ -1,17 +1,25 @@
 function afq = AFQ_run_sge(afq, numsubs, v)
 % Run AFQ using a sun grid engine to parallelize the computations
 %
-% afq = AFQ_run_sge(afq, numsubs)
+% afq = AFQ_run_sge(afq, numsubs, v)
+%
+% Run afq on a compute cluster (or with matlab parallel toolbox). This
+% function is brittle and written for specific clusters I have used. Also
+% it will not work with 2 groups os subjects (patients and controls), but
+% requires only 1 subject group.
+%
+% Inputs:
+% afq     - AFQ structure
+% numsubs - Subjects to run. Defaults to all
+% v       - Scaler specifying which grid call to use. v=1 for sgerun1,
+%           v=2 for sgerun2 (proclus) and v=3 for local cluseter using
+%           matlabpool
 
 % which sge command to use
 if notDefined('v')
     v = 2;
 end
-% set a new outdir
-afq.params.outdir = fullfile(afq.params.outdir,'sge');
-if ~exist(afq.params.outdir)
-    mkdir(afq.params.outdir);
-end
+
 % Do not compute norms
 afq = AFQ_set(afq,'computenorms',0);
 % Get the number of subjects
@@ -19,35 +27,73 @@ if ~exist('numsubs','var') || isempty(numsubs)
     numsubs = 1:AFQ_get(afq,'numsubs');
 end
 
-% Loop over the subjects and push the processing to the grid
-for ii = numsubs
-   % Set which subject to send to the grid
-   afq = AFQ_set(afq,'runsubs',ii);
-   % Set the name of the saved file
-   afq = AFQ_set(afq, 'outname', ['afq_' num2str(ii)]);
-   % Name the job
-   jobname{ii} = sprintf('AFQ%d_%d',ii,round(rand*1000));
-   % Process this subject on the grid
-   if v == 2
-       sgerun2('AFQ_run([],[],afq);',jobname{ii},1);
-   elseif v == 1
-       sgerun('AFQ_run([],[],afq);',jobname{ii},1);
-   end
+% if an output directory isn't in the afq structure then set one
+if isempty(afq.params.outdir)
+    afq = AFQ_set(afq,'outdir','~/sge');
+    if ~exist(afq.params.outdir,'dir')
+        mkdir(afq.params.outdir);
+    end
+    fprintf('\n No output directory specified. Writing outputs to %s',afq.params.outdir);
+    % If outdir was already defined than make an sge directory within
+else
+    afq.params.outdir = fullfile(afq.params.outdir,'sge');
+    if ~exist(afq.params.outdir,'dir')
+        mkdir(afq.params.outdir);
+    end
 end
 
-% Wait for them to be done
-done = 0;
-fprintf('\n\n --Waiting for the grid to finish--\n\n')
-while done == 0
-   % wait 5 seconds
-    pause(5) 
-    % Check the job status
-    [~,stat] = system('qstat');
-    % Search for jobs with AFQ in the title
-    jobnums = strfind(stat,'AFQ');
-    if isempty(jobnums)
-        done = 1;
+%% Use sun grid engine (SGE) to run subjects in parallel
+% Loop over the subjects and push the processing to the grid
+if v == 1 || v == 2
+    for ii = numsubs
+        % Set which subject to send to the grid
+        afq = AFQ_set(afq,'runsubs',ii);
+        % Set the name of the saved file
+        afq = AFQ_set(afq, 'outname', ['afq_' num2str(ii)]);
+        % Name the job
+        jobname{ii} = sprintf('AFQ%d_%d',ii,round(rand*1000));
+        % Process this subject on the grid
+        if v == 2
+            sgerun2('AFQ_run([],[],afq);',jobname{ii},1);
+        elseif v == 1
+            sgerun('AFQ_run([],[],afq);',jobname{ii},1);
+        end
     end
+    
+    % Wait for jobs to be done
+    done = 0;
+    fprintf('\n\n --Waiting for the grid to finish--\n\n')
+    while done == 0
+        % wait 5 seconds
+        pause(5)
+        % Check the job status
+        [~,stat] = system('qstat');
+        % Search for jobs with AFQ in the title
+        jobnums = strfind(stat,'AFQ');
+        if isempty(jobnums)
+            done = 1;
+        end
+    end
+end
+
+%% Use parallel toolbox to take advantage of local cores
+if v == 3
+    % If an existing pool is open, then close it
+    if matlabpool('size') > 0
+        matlabpool('close');
+    end
+    % Open a new pool
+    matlabpool
+    parfor ii = numsubs
+        % Set which subject to send to the grid
+        afq_i = AFQ_set(afq,'runsubs',ii);
+        % Set the name of the saved file
+        afq_i = AFQ_set(afq_i, 'outname', ['afq_' num2str(ii)]);
+        % run AFQ
+        AFQ_run([],[],afq_i);
+    end
+    % close the pool
+    matlabpool close;
 end
 
 %% Reconstruct AFQ structure
@@ -57,8 +103,8 @@ afqfull = afq;
 for ii = numsubs
     afqfile = dir(fullfile(afqfull.params.outdir,['afq*_' num2str(ii) '.mat']));
     if ~isempty(afqfile)
-    load(fullfile(afq.params.outdir,afqfile.name));
-    afqfull = AFQ_CombineSgeRuns(afqfull, afq, ii);
+        load(fullfile(afq.params.outdir,afqfile.name));
+        afqfull = AFQ_CombineSgeRuns(afqfull, afq, ii);
     else
         fprintf('\nNo afq file found for subject %d',ii)
     end
@@ -72,6 +118,8 @@ afq = afqfull;
 save(fullfile(afq.params.outdir,['AFQ_sge_' date]),'afq');
 
 %% Delete all the jobs to not waste space
-for ii = 1:length(jobname)
-    delete(fullfile('~/sgeoutput',['job_' jobname{ii} '*']));
+if v == 1 || v == 2
+    for ii = 1:length(jobname)
+        delete(fullfile('~/sgeoutput',['job_' jobname{ii} '*']));
+    end
 end
