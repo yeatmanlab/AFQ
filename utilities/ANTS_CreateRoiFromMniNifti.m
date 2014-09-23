1261d1176f2d0acdde4dd90e05f918834da85f88
jyeatman <jyeatman@stanford.edu>
2014-09-23 16:22:25 -0700

    fixed the ANTS roi creation to use the computed affine. For some reason ignoring this did not seem to cause a problem in most cases.

--- utilities/ANTS_CreateRoiFromMniNifti.m
+++ utilities/ANTS_CreateRoiFromMniNifti.m
@@ -1,12 +1,16 @@
 function roi = ANTS_CreateRoiFromMniNifti(roiNifti, warpNifti,refNifti, outfile)
 % Transform a nifti roi saved in MNI space to an individuals brain
 
+% Find the affine associated with the warp
+f = strfind(warpNifti,'Inverse')-1;
+affineXform = [warpNifti(1:f) 'Affine.txt'];
+
 % Create the command to call ANTS
 if exist('refNifti','var') && ~isempty(refNifti)
     % If a reference image was provided then reslice the roi to match this
-    cmd = sprintf('WarpImageMultiTransform 3 %s %s -R %s --use-NN %s', roiNifti, outfile, refNifti, warpNifti);
+    cmd = sprintf('WarpImageMultiTransform 3 %s %s -R %s --use-NN -i %s %s', roiNifti, outfile, refNifti, affineXform, warpNifti);
 else
-    cmd = sprintf('WarpImageMultiTransform 3 %s %s --use-NN %s', roiNifti, outfile, warpNifti);
+    cmd = sprintf('WarpImageMultiTransform 3 %s %s --use-NN -i %s %s', roiNifti, outfile, affineXform, warpNifti);
 end
 % excecute it
 system(cmd);
@@ -15,7 +19,7 @@ system(cmd);
 roi = dtiRoiFromNifti(outfile,[],prefix(prefix(outfile)),'mat');
 
 % Clean the ROI
-roi = dtiRoiClean(roi,3,{'dilate' 'fillHoles'});
+roi = dtiRoiClean(roi,3,{'fillHoles'});
 
 % Save the ROI
 dtiWriteRoi(roi,prefix(prefix(outfile)));
