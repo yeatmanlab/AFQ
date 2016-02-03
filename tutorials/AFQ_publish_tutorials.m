6f6797dcacbe90d8c09a784534d314b258ab6e48
jyeatman <jyeatman@stanford.edu>
2016-02-03 13:37:58 -0800

    updates

--- tutorials/AFQ_publish_tutorials.m
+++ tutorials/AFQ_publish_tutorials.m
@@ -2,6 +2,11 @@ function AFQ_publish_tutorials(remotename)
 % Publish all the AFQ tutorials as html pages push to gh-pages
 %
 % AFQ_publish_tutorials(remotename)
+%
+% inputs:
+% remotename = string for a git remote repository. Default is 'origin'
+%
+% Written by JDY, Feb, 2016
 
 if ~exist('remotename', 'var') || isempty(remotename)
     remotename = 'origin';
@@ -12,15 +17,19 @@ tdir = fullfile(afqdir,'tutorials');
 % List of tutorials to publish
 tlist = {'AFQ_example.m'};
 
+% Publish tutorial list
 opts.format = 'html';
-opts.outputDir = tdir;
+opts.outputDir = fullfile(tdir,'htmltmp');
 for ii = 1:length(tlist)
     publish(tlist{ii},opts); 
 end
 
-% Checkout gh-pages branch
+% Checkout gh-pages branch add tutorials, push to github and change back to
+% master branch
 cd(afqdir)
 system('git checkout gh-pages')
+system(sprintf('mv %s %s',fullfile(tdir,'htmltmp','*'),tdir))
+system(sprintf('rm -d %s %s',fullfile(tdir,'htmltmp')))
 system('git add tutorials')
 system('git commit -m ''changes to tutorials''')
 system(sprintf('git push %s gh-pages', remotename))
