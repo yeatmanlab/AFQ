a7f42a61c364efea5de74c63e1a6a66887fd3306
jyeatman <jyeatman@uw.edu>
2018-12-21 09:02:19 -0800

    updated roi drawing to work for volume rois and write as nifti image

--- 3Dmesh/AFQ_meshDrawRoi.m
+++ 3Dmesh/AFQ_meshDrawRoi.m
@@ -1,38 +1,71 @@
-function [coords, indices, bin, msh, spline_points, spline_meshpoints, spline_meshindices] = AFQ_meshDrawRoi(msh, dilate, fh)
+function [mshRoi, vRoi, msh] = AFQ_meshDrawRoi(msh, dilate, voldata, fill_range, fh, outname)
 %
-% [coords, indices, bin, msh, spline_points, spline_meshpoints, spline_meshindices] = AFQ_meshDrawRoi(msh, dilate, fh)
+% [mshRoi, vRoi, msh] = AFQ_meshDrawRoi(msh, dilate, voldata, fill_range, fh)
 %
 %
 % Inputs:
 %
-% msh    - AFQ mesh structure. See AFQ_meshCreate.m
-% dilate - How much to dilate each point. Default is 0 which means no
-%          dilation
-% fh     - figure handle if you already have a mesh window open. Default: open
-%          a new figure window
+% msh       - AFQ mesh structure. See AFQ_meshCreate.m
+% dilate    - How much to dilate each point. Default is 0 which means no
+%             dilation
+% voldata   - Nifti with fMRI data (contrast map or p-values)
+% fill_range- Range of values to fill in the volume
+% fh        - figure handle if you already have a mesh window open. Default: open
+%             a new figure window
+% outname   - If defined, will save ROIs with this name and path
 %
 % Outputs:
 %
-% coords            - The coordinates of each point drawn on the mesh
-% indices           - Indices for the vertices within msh.tr.vertices
-% bin               - A binary vector denoting which vertices in
-%                     msh.tr.vertices are within in ROI
-% msh               - A mesh is returned with the ROI drawn on it
-% spline_points     - Points from a spline fit to the points
-% spline_meshpoints - Those spline points projected onto the mesh surface
-% spline_meshindices- Indices into msh.tr.vertices that correspond to
-%                     spline_meshpoints.
+% mshRoi.coords            - The coordinates of each point drawn on the mesh. If a
+%                            volume fill roi then it will be a nifti image with
+%                            the dimensions of the volume.
+% mshRoi.indices           - Indices for the vertices within msh.tr.vertices
+% mshRoi.bin               - A binary vector denoting which vertices in
+%                            msh.tr.vertices are within in ROI
+% mshRoi.msh               - A mesh is returned with the ROI drawn on it
+% mshRoi.spline_points     - Points from a spline fit to the points
+% mshRoi.spline_meshpoints - Those spline points projected onto the mesh surface
+% mshRoi.spline_meshindices- Indices into msh.tr.vertices that correspond to
+%                            spline_meshpoints.
+% vRoi                     - Nifti volume ROI
+% msh                      - Return the mesh with the ROI drawn on it
 %
-% Example:
+% Example 1 Draw line on mesh:
 %
 % msh = AFQ_meshCreate;
 % [msh.tr.vertices, msh.tr.faces] = read_surf('/mnt/diskArray/projects/freesurfer/fsaverage/surf/lh.white');
 % msh.tr.faces = msh.tr.faces + 1;
 % msh.tr.FaceVertexCData = repmat([.8 .7 .6],size(msh.tr.vertices,1),1);
 % msh.tr = smoothpatch(msh.tr,[],20);
-% [coords, indices, bin, msh] = AFQ_meshDrawRoi([],msh, 7)
+% [mshRoi,[], msh] = AFQ_meshDrawRoi([],msh, 7)
 % AFQ_RenderCorticalSurface(msh);
+%
+% % Example 2 - Create fMRI based ROI and save as nifti image:
+%
+% voldata = 'functionalOverlay-20-Dec-2018.nii.gz';
+% t1class = 't1_class.nii.gz';
+% im = niftiRead(t1class);
+% msh = AFQ_meshCreate(t1class);
+% fill_range = [2 inf];
+% [~,vRoi] = AFQ_meshDrawRoi(msh, [], voldata, fill_range,[], '/Users/jyeatman/Documents/vwfa_roi');
+
+% Check if ROI should be filled in a volume or not
+if ~exist('voldata', 'var') || isempty(voldata)
+    drawtype = 'line';
+else
+    drawtype = 'volfill';
+end
+
+% Check if we should work with an open figure window
 if ~exist('fh', 'var') || isempty(fh)
+    if strcmp('volfill',drawtype)
+        if ischar(voldata), voldata = niftiRead(voldata); end
+        voldata.fname = 'voldata.nii.gz';
+        msh = AFQ_meshColor(msh,'overlay',voldata, 'thresh',fill_range);
+        % Create a binarized volume
+        v = voldata.data > fill_range(1) & voldata.data<fill_range(2);
+        vRoi = voldata; vRoi.data = zeros(size(v));
+    end
     [p,~,lh]=AFQ_RenderCorticalSurface(msh);
     title(sprintf('Press RETURN to exit\nArrows to rotate; < > to zoom'))
     view(180,-90); camlight(lh,'infinite');
@@ -42,7 +75,10 @@ if ~exist('fh', 'var') || isempty(fh)
     % get current vertices
     curver = msh.vertex.current;
 end
-datacursormode on
+
+%% Draw ROI
+
+datacursormode on;
 dcmObj = datacursormode(fh);
 set(dcmObj,'SnapToDataVertex','on','Enable','on','DisplayStyle','window');
 ii = 0; sp = []; spline_meshpoints = []; currkey= 0;
@@ -54,23 +90,37 @@ while currkey==0
     if press == 0
         ii = ii+1;
         point = getCursorInfo(dcmObj);
-        coords(ii,:) = point.Position
+        coords(ii,:) = point.Position;
         plot3(coords(ii,1),coords(ii,2),coords(ii,3),'ko','markerfacecolor','k');
         % get index of the coords
         [~,indices(ii)] = ismember(coords(ii,:), msh.tr.vertices, 'rows');
-        % Fit a spline to the points
-        if ii >1
-            cs = cscvn(coords'); spline_points = fnplt(cs)';
-            % Map spline points onto mesh
-            [spIndices, spbestSqDist] = nearpoints(spline_points',msh.tr.vertices');
-            spline_meshpoints = msh.tr.vertices(spIndices,:,:);
-            delete(sp);
-            sp = plot3(spline_meshpoints(:,1), spline_meshpoints(:,2), spline_meshpoints(:,3),...
-                '-b','linewidth',4);
-            % get index of the coords
-            [~,spline_meshindices(ii)] =   ismember(spline_meshpoints(ii,:), msh.tr.vertices, 'rows');
+        switch(drawtype)
+            case 'volfill'
+                imcoords = ceil(mrAnatXformCoords(voldata.qto_ijk, coords(ii,:)));
+                vRoi.data = bwselect3(v,imcoords(2),imcoords(1),imcoords(3)) | vRoi.data>0;
+                if sum(vRoi.data(:)) == 0
+                    fprintf('\nNO VOLUME DATA AT SELECTED VERTEX.\nTRY ANOTHER SPOT\n');
+                else
+                    roiMsh = AFQ_meshCreate(vRoi,'color',[0 1 0],'smooth',1); patch(roiMsh.tr); shading('interp'); lighting('gouraud');
+                end
+                % Fit a spline to the points
+                if ii >1
+                    cs = cscvn(coords'); spline_points = fnplt(cs)';
+                    % Map spline points onto mesh
+                    [spIndices, spbestSqDist] = nearpoints(spline_points',msh.tr.vertices');
+                    spline_meshpoints = msh.tr.vertices(spIndices,:,:);
+                    % get index of the coords
+                    [~,spline_meshindices(ii)] = ismember(spline_meshpoints(ii,:), msh.tr.vertices, 'rows');
+                end
+                
+            case 'line'
+                % Draw on the cortical surface if a line ROI
+                if ii >1
+                    delete(sp);
+                    sp = plot3(spline_meshpoints(:,1), spline_meshpoints(:,2), spline_meshpoints(:,3),...
+                        '-b','linewidth',4);
+                end
         end
-        
         % Check for button presses and either rotate camera or exit loop
     elseif press == 1
         keypress=get(gcf,'CurrentKey');
@@ -107,6 +157,7 @@ while currkey==0
     
 end
 
+vRoi.data = uint8(vRoi.data);
 msh = AFQ_meshSet(msh,'vertices',curver);
 hold('off');
 
@@ -127,6 +178,24 @@ msh.tr.FaceVertexCData(indices,:) = repmat([1 0 0],length(indices),1);
 % Make a logical vector out of the indices
 bin = zeros(size(msh.tr.vertices,1),1);
 bin(indices) = 1;
+
+% If the ROI is just one point then splines will be empty
+if size(coords,1) == 1
+    spline_points = coords;
+    spline_meshpoints = coords;
+    spline_meshindices = indices;
+end
+% Put all the variables into a struct
+mshRoi = struct('coords', coords, 'indices', indices, 'bin', bin, ...
+ 'spline_points',spline_points, 'spline_meshpoints', spline_meshpoints, ...
+ 'spline_meshindices', spline_meshindices);
+
+% Save if desired
+if exist('outname', 'var') && ~isempty(outname)
+    vRoi.fname = [outname '.nii.gz'];
+    niftiWrite(vRoi);
+    save(outname, 'mshRoi');
+end
 
 return
 
