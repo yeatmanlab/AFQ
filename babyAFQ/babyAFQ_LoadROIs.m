f8b9af9bdae0f320599353c397304ac3f440ffcd
Mareike Grotheer <mareikegrotheer@gmail.com>
2020-08-26 11:33:32 -0700

    This is the first commit for babyAFQ, which is still work in progress. This commit introduces a new folder "babyAFQ" which includes 1) the UNC baby template (with permission), 2) way-point ROIs and probability maps in UNC Neo space and 3) functions for segmenting baby connectomes using this infant specific template rather than the adult MNI template.

---
+++ babyAFQ/babyAFQ_LoadROIs.m
@@ -0,0 +1,67 @@
+function [roi1, roi2] = AFQ_LoadROIs(fgNumber,sub_dir, afq,roiDir)
+
+%%%%%%%%%%%%%%%%%%%%
+% This is an infant implementation of AFQ_LoadROIs
+% The documentation for the original function in enclosed below
+
+% In brief, the implemented infant-specfic changes are:
+% 1) additional ROIs were added, so that ROIs for pAF and mdLF can also be
+% loaded with this function
+
+% InfantAFQ was written by Mareike Grotheer in 2020
+
+%%%%%%%%%%%%%%%%%%%%%
+
+% Load the two ROIs used to define a fiber group
+%
+% [roi1 roi2] = AFQ_LoadROIs(fgNumber,sub_dir. [afq])
+%
+% Inputs:
+% fgNumber = The number of the fiber group. For example 1 is left ATR and
+%            20 is right arcuate.
+% sub_dir  = A path to the subjects directory
+% afq      - An afq structure. This is optional but is needed if it is a
+%            fiber group not in the mori groups
+%
+% Outputs:
+% roi1     = First roi defining the tract
+% roi2     = Second roi defining the tract
+%
+% Written by Jason D. Yeatman 1/31/2012
+
+% If no afq structure was passed in it is fine for known ROIs
+if ~exist('afq','var') || isempty(afq) && fgNumber <=25
+    roi1Names={'ATR_roi1_L', 'ATR_roi1_R','CST_roi1_L','CST_roi1_R','CGC_roi1_L','CGC_roi1_R'...
+        'HCC_roi1_L','HCC_roi1_R','FP_L','FA_L','IFO_roi1_L','IFO_roi1_R','ILF_roi1_L','ILF_roi1_R'...
+        'SLF_roi1_L','SLF_roi1_R','UNC_roi1_L','UNC_roi1_R','SLF_roi1_L','SLF_roi1_R'...
+        'MdLF_roi1_L','MdLF_roi1_R','SLFt_roi2_L','SLFt_roi2_R'};
+    roi2Names={'ATR_roi2_L', 'ATR_roi2_R','CST_roi2_L','CST_roi2_R','CGC_roi2_L','CGC_roi2_R'...
+        'HCC_roi2_L','HCC_roi2_R','FP_R','FA_R','IFO_roi2_L','IFO_roi2_R','ILF_roi2_L','ILF_roi2_R'...
+        'SLF_roi2_L','SLF_roi2_R','UNC_roi2_L','UNC_roi2_R','SLFt_roi2_L','SLFt_roi2_R'...
+        'ILF_roi2_L','ILF_roi2_R','LH_Parietal', 'RH_Parietal'};
+    
+    roi1path = fullfile(roiDir,[roi1Names{fgNumber} '.mat']);
+    roi2path = fullfile(roiDir,[roi2Names{fgNumber} '.mat']);
+    if exist(roi1path,'file') && exist(roi2path,'file')
+        roi1=dtiReadRoi(roi1path);
+        roi2=dtiReadRoi(roi2path);
+    else
+        error('ROI does not exist for %s',sub_dir)
+    end
+else
+    % It doesn't matter which subject number because all we need is the roi
+    % name
+    [~,roi1Name] = fileparts(AFQ_get(afq,'roi1',fgNumber,1));
+    [~,roi2Name] = fileparts(AFQ_get(afq,'roi2',fgNumber,1));
+
+    roi1path = fullfile(roiDir,[roi1Name '.mat']);
+    roi2path = fullfile(roiDir,[roi2Name '.mat']);
+    if exist(roi1path,'file') && exist(roi2path,'file')
+        roi1=dtiReadRoi(roi1path);
+        roi2=dtiReadRoi(roi2path);
+    else
+        error('ROI does not exist for %s',sub_dir)
+    end
+end
+
+return
---
+++ babyAFQ/babyAFQ_Segment_PostArcuate.m
@@ -0,0 +1,179 @@
+function [L_FG, R_FG, L_roi_1, L_roi_2, R_roi_1, R_roi_2] = babyAFQ_Segment_PostArcuate(dt, wholebrainFG, varargin)
+
+%%%%%%%%%%%%%%%%%%%%
+% This is an infant implementation of AFQ_Segment_PostArcuate
+% The documentation for the original function in enclosed below
+
+% In brief, the implemented infant-specfic changes are:
+% 1) pAF and VOF ROIs are already warped to individual subject space in
+% babyAFQ_SegmentFiberGroup and just loaded here
+% 2) posterior AF ROIs are taken from babyAFQROIs folder
+
+% InfantAFQ was written by Mareike Grotheer in 2020
+
+%%%%%%%%%%%%%%%%%%%%%
+% Original documentation:     
+
+
+% Define the posterior segment of the arcuate from a wholebrain fiber group
+%
+% [L_FG, R_FG, L_roi_1, L_roi_2, R_roi_1, R_roi_2] = AFQ_Segment_PostArcuate(dt, wholebrainFG, varargin)
+%
+% This function will define the left and right posterior segment of the
+% arcuate fasciculus. 
+%
+% Arguments:
+% 'showfibers' - Locigal. Whether or not to render figures of the fibers
+% 'saveFiles'  - Logical. Whether or not to save the fibers and ROIs
+%
+% Copyright Jason D. Yeatman December 2012
+%% Argument checking
+if ~exist('dt','var') || isempty(dt)
+    error('dt6 file is needed');
+elseif ischar(dt)
+    dt = dtiLoadDt6(dt);
+end
+if ~exist('wholebrainFG','var') || isempty(wholebrainFG)
+    fprintf('Wholebrain fiber group was not supplied. Tracking fibers');
+    wholebrainFG = AFQ_WholebrainTractography(dt);
+elseif ischar(wholebrainFG) && exist(wholebrainFG,'file')
+    wholebrainFG = dtiReadFibers(wholebrainFG);
+elseif ischar(wholebrainFG) && ~exist(wholebrainFG,'file')
+    fprintf('Wholebrain fiber group was not found. Tracking fibers');
+    wholebrainFG = AFQ_WholebrainTractography(dt);
+end
+if ~exist('sub_dir','var') || isempty(sub_dir)
+    sub_dir = fileparts(dt.dataFile);
+end
+
+if ~isempty(varargin)
+    % Check if inverse deformation was passed in so computations can be skipped
+    for ii = 1:length(varargin)
+        if isstruct(varargin{ii}) && isfield(varargin{ii},'deformX')...
+                && isfield(varargin{ii},'coordLUT')...
+                && isfield(varargin{ii},'inMat')
+            invDef= varargin{ii};
+        end
+    end
+    % Check if rois and fibers should be saved
+    for ii = 1:length(varargin)
+        if strcmpi('saveFiles',varargin{ii}) && length(varargin) > ii
+            saveFiles = varargin{ii+1};
+        end
+    end
+end
+
+% Save figures by default
+if ~exist('saveFiles','var') || isempty(saveFiles)
+    saveFiles = 1;
+end
+
+%% Create ROIs and segment the posterior segment of the arcuate
+% Path to the templates directory
+% tdir = fullfile(fileparts(which('mrDiffusion.m')), 'templates');
+% template = fullfile(tdir,'MNI_EPI.nii.gz');
+% 
+% % Path to the VOF ROIs in MNI space
+% AFQbase = AFQ_directories;
+% % L_roi_2 = fullfile(AFQbase,'templates', 'L_Arcuate_PostSegment.nii.gz');
+% % R_roi_2 = fullfile(AFQbase,'templates', 'R_Arcuate_PostSegment.nii.gz');
+% L_roi_2 = fullfile(AFQbase,'templates', 'L_Parietal.nii.gz');
+% R_roi_2 = fullfile(AFQbase,'templates', 'R_Parietal.nii.gz');
+% % L_roi_2 = fullfile(AFQbase,'templates', 'L_LateralParietal.nii.gz');
+% % R_roi_2 = fullfile(AFQbase,'templates', 'R_LateralParietal.nii.gz');
+% 
+% % Compute spatial normalization
+% if ~exist('invDef','var') || isempty(invDef)
+%     [sn, Vtemplate, invDef] = mrAnatComputeSpmSpatialNorm(dt.b0, dt.xformToAcpc, template);
+% end
+% 
+% % Load up template ROIs in MNI space and transform them to the subjects
+% % native space.  dtiCreateRoiFromMniNifti also saves the ROIs
+% [~, ~, L_roi_2]=babyFatDtiCreateRoiFromMniNifti(dt.dataFile, L_roi_2, invDef, 1);
+% [~, ~, R_roi_2]=babyFatDtiCreateRoiFromMniNifti(dt.dataFile, R_roi_2, invDef, 1);
+% Name the ROIs
+L_roi_2.name = 'L_Parietal';
+R_roi_2.name = 'R_Parietal';
+
+% Load up predefined ROIs for the Arcuate fasciculus. These are computed in
+% AFQ_SegmentFiberGroups
+[L_roi_not, L_roi_1] = babyAFQ_LoadROIs(19,sub_dir,[],fullfile(sub_dir,'babyAFQROIs'));
+[R_roi_not, R_roi_1] = babyAFQ_LoadROIs(20,sub_dir,[],fullfile(sub_dir,'babyAFQROIs'));
+[L_roiNotUsed, L_roi_2] = babyAFQ_LoadROIs(23,sub_dir,[],fullfile(sub_dir,'babyAFQROIs'));
+[R_roiNotUsed, R_roi_2] = babyAFQ_LoadROIs(24,sub_dir,[],fullfile(sub_dir,'babyAFQROIs'));
+
+% To eliminate fibers that head too far anterior we will create a full
+% plane at the y coordinate of the anterior slf roi (around the central
+% sulcus) and remove fibers that intersect this ROI
+L_roi_not = dtiRoiMakePlane([-60,mean(L_roi_not.coords(:,2)),-60; 60,mean(L_roi_not.coords(:,2)),80]);
+R_roi_not = dtiRoiMakePlane([-60,mean(R_roi_not.coords(:,2)),-60; 60,mean(R_roi_not.coords(:,2)),80]);
+
+% Intersect the wholebrain fiber group with the ROIs to retain only fibers
+% that correspond to the posterior segment of the arcuate
+L_FG = dtiIntersectFibersWithRoi([],'and',[],L_roi_1,wholebrainFG);
+
+% We want fibers that are traveling superior to inferior as they pass
+% through the first ROI.  To do this we will restrict the ROI to voxels
+% with superior-inferior orientation and repeate this process for planes
+% directly above and below the ROI
+for ii = -3:3
+    roi_tmp = L_roi_1;
+    % Shift the roi
+    roi_tmp.coords(:,3) = roi_tmp.coords(:,3) + ii;
+    % Compute the PDD at each point in each ROI
+    roi_pdd = dtiGetValFromTensors(dt.dt6, roi_tmp.coords, inv(dt.xformToAcpc), 'pdd');
+    % Find every coordinate in each VOF ROI where the PDD is in the Z direction
+    [~, roi_pddZ] = max(abs(roi_pdd),[],2);
+    roi_pddZ = roi_pddZ == 3;
+    % Retain VOF ROI coordinates that have a PDD in the Z direction
+    roi_tmp.coords = roi_tmp.coords(roi_pddZ,:);
+    L_FG = dtiIntersectFibersWithRoi([],'and',[],roi_tmp,L_FG);
+end
+%L_FG = dtiIntersectFibersWithRoi([],'and',[],L_roi_2,L_FG);
+% Make sure endpoints are in the parietal lobe
+L_FG = dtiIntersectFibersWithRoi([],'endpoints',4,L_roi_2,L_FG);
+% And that fibers do not head to far anterior
+L_FG = dtiIntersectFibersWithRoi([],'not',[],L_roi_not,L_FG);
+
+% Same for the right hemisphere fiber group
+R_FG = dtiIntersectFibersWithRoi([],'and',[],R_roi_1,wholebrainFG);
+for ii = -3:3
+    roi_tmp = R_roi_1;
+    % Shift the roi
+    roi_tmp.coords(:,3) = roi_tmp.coords(:,3) + ii;
+    % Compute the PDD at each point in each ROI
+    roi_pdd = dtiGetValFromTensors(dt.dt6, roi_tmp.coords, inv(dt.xformToAcpc), 'pdd');
+    % Find every coordinate in each VOF ROI where the PDD is in the Z direction
+    [~, roi_pddZ] = max(abs(roi_pdd),[],2);
+    roi_pddZ = roi_pddZ == 3;
+    % Retain VOF ROI coordinates that have a PDD in the Z direction
+    roi_tmp.coords = roi_tmp.coords(roi_pddZ,:);
+    R_FG = dtiIntersectFibersWithRoi([],'and',[],roi_tmp,R_FG);
+end
+%R_FG = dtiIntersectFibersWithRoi([],'and',[],R_roi_2,R_FG);
+% Make sure endpoints are in the parietal lobe
+R_FG = dtiIntersectFibersWithRoi([],'endpoints',4,R_roi_2,R_FG);
+% And that fibers do not head to far anterior
+R_FG = dtiIntersectFibersWithRoi([],'not',[],R_roi_not,R_FG);
+
+% Name the fiber groups
+L_FG.name = 'L_Arcuate_Posterior';
+R_FG.name = 'R_Arcuate_Posterior';
+
+% Save the segmented fiber groups and ROIs
+if saveFiles == 1
+    dtiWriteFiberGroup(L_FG,fullfile(sub_dir,'fibers',L_FG.name));
+    dtiWriteFiberGroup(R_FG,fullfile(sub_dir,'fibers',R_FG.name));
+    %dtiWriteRoi(L_roi_2,fullfile(sub_dir,'babyAFQROIs',L_roi_2.name));
+    %dtiWriteRoi(R_roi_2,fullfile(sub_dir,'babyAFQROIs',R_roi_2.name));
+end
+
+% Show the fiber group if desired
+if sum(strcmpi('showfibers', varargin)) > 0
+    AFQ_RenderFibers(L_FG,'color',[0 0 1],'numfibers',300);
+    b0 = readFileNifti(dt.files.b0);
+    AFQ_AddImageTo3dPlot(b0,[-30 0 0]);
+    AFQ_RenderRoi(L_roi_1);
+end
+
+return
